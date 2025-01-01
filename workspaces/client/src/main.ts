ef22e3213b7d5a0bc1ca1c5c2fb8763c3e3fa181
valenthyne-ut <valenthyne.ut@gmail.com>
2025-01-01 17:58:32 +0000

    Simplify Vue example SPA

--- workspaces/client/src/main.ts
+++ workspaces/client/src/main.ts
@@ -1,14 +1,15 @@
-import "./assets/main.css";
+import "./assets/preflight.scss";
+import "./assets/main.scss";
 
 import { createApp, type Component } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
-import router from "./router";
+// import router from "./router";
 
 const app = createApp(App as Component);
 
 app.use(createPinia());
-app.use(router);
+// app.use(router);
 
 app.mount("#app");
